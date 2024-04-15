d7724355e13d6450aec5d7e23fd7a89b6825e0ee
Nitika Rawat <93915470+nitikarawat82@users.noreply.github.com>
2024-04-15 11:27:34 +0530

    Add files via upload

---
+++ Pods/Target Support Files/Pods-IOScollegeApp-IOScollegeAppUITests/Pods-IOScollegeApp-IOScollegeAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOScollegeApp_IOScollegeAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IOScollegeApp_IOScollegeAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-IOScollegeAppTests/Pods-IOScollegeAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOScollegeAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_IOScollegeAppTests
+@end
